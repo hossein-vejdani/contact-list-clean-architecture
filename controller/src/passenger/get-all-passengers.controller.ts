4396681260c56e2d991dc18e0501c8bd9c139f42
Seyyed Hossein Vejdani <shvejdani.alibaba@gmail.com>
2022-09-10 23:46:14 +0430

    feat: define get all passenger controller

---
+++ controller/src/passenger/get-all-passengers.controller.ts
@@ -0,0 +1,16 @@
+import { GetAllPassengersService } from '@contact-management/core'
+import { GetAllPassengersPresenter } from '@contact-management/infra'
+console.log('hello')
+
+export class GetAllPassengersController {
+    service: GetAllPassengersService
+    public readonly presenter: GetAllPassengersPresenter
+    constructor() {
+        this.presenter = new GetAllPassengersPresenter()
+        this.service = new GetAllPassengersService(this.presenter)
+    }
+
+    getAll(...args: Parameters<typeof this.service.execute>) {
+        this.service.execute(...args)
+    }
+}
