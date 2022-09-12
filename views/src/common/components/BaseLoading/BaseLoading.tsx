00ed09d7ca1aeeb8061a829212f088113a15dd43
Seyyed Hossein Vejdani <shvejdani.alibaba@gmail.com>
2022-09-12 11:25:53 +0430

    feat: create base loading component

---
+++ views/src/common/components/BaseLoading/BaseLoading.tsx
@@ -0,0 +1,8 @@
+import './BaseLoading.scss'
+const BaseLoading = () => {
+    return (
+        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default BaseLoading
