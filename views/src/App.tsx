4b43a2b94e2021b95eac0c6c1b8e84f2a4e08b52
Seyyed Hossein Vejdani <shvejdani.alibaba@gmail.com>
2022-09-12 11:25:09 +0430

    feat: global styles

--- views/src/App.tsx
+++ views/src/App.tsx
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from 'react-router-dom'
+import { useGlobalHTTPErrorHandler } from './common/hooks/useGlobalHTTPErrorHandler.hook';
+import { routes } from './router/routes';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
+  const page = useRoutes(routes);
+  useGlobalHTTPErrorHandler()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {page}
+      <ToastContainer />
+    </main>
   );
 }
 
